70deb0deb622d93931154202b72a9ef860cf0707
vov77 <vov1977@mail,ru>
2020-01-10 19:10:02 +0300

    DZ1

---
+++ src/main/java/sandbox/javafortesters/Point.java
@@ -0,0 +1,36 @@
+package sandbox.javafortesters;
+
+public class Point {
+  double x;
+  double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+
+  public double getX() {
+    return x;
+  }
+
+  public void setX(double x) {
+    this.x = x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public void setY(double y) {
+    this.y = y;
+  }
+
+  public static double distance (Point p1, Point p2) {
+    double x = (p1.x-p2.x)*(p1.x-p2.x);
+    double y = (p1.y - p2.y)*(p1.y - p2.y);
+    return Math.sqrt(x+y);
+  }
+
+
+}
--- src/main/java/sandbox/javafortesters/TestProg1.java
+++ src/main/java/sandbox/javafortesters/TestProg1.java
@@ -3,6 +3,14 @@ package sandbox.javafortesters;
 public class TestProg1 {
   public static void main(String[] args) {
     System.out.println("testtesttest");
+    Point p3 = new Point(100,50);
+    Point p4 = new Point(100,50);
+
+    System.out.println(Point.distance(p4, p3));
+
+
   }
+
+
 
 }
