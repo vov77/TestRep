8a60479d56d5d776e0547dbafb96713f1c2e057d
vov77 <vov1977@mail,ru>
2020-01-10 17:08:55 +0300

    Package created

--- sandbox/src/main/java/TestProg.java
+++
@@ -1,5 +0,0 @@
-public class TestProg {
-    public static void main(String[] args) {
-        System.out.println("Privetik");
-    }
-}
---
+++ src/main/java/sandbox/javafortesters/TestProg1.java
@@ -0,0 +1,8 @@
+package sandbox.javafortesters;
+
+public class TestProg1 {
+    public static void main(String[] args) {
+        System.out.println("testtesttest");
+    }
+
+}
